4d4d0843c17b9f21fef3e3be76857db746cdfc55
Priyanshu Rajput <priyanshurajput@Priyanshus-MacBook-Air.local>
2025-09-27 12:23:00 +0530

    production

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ Now answer the user question: {query}
 
 if __name__ == "__main__":
     print("Starting Flask server on http://127.0.0.1:5000")
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
